6ae59b36196a72aea4b41964e15634a779abb993
Vinicius <viniciuspascoal013@gmail.com>
2026-08-04 09:01:00 -0300

    clean header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Header from '../components/ui/Header'
 import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
